e42cb93b76419f64d01840b76be9651c6394f093
zebnouf <66158834+zebnouf@users.noreply.github.com>
2020-06-14 00:41:10 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+	function toggle(){
+		var nav = document.getElementById('nav');
+		nav.classList.toggle('active');
+		var burger = document.getElementById("burgerct");
+		burger.classList.toggle('active');
+	};
+	
